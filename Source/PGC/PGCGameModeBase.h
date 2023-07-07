0237dd6be31f47b92776ce50effd04a557c6f255
Alexandre Lima <alexandrelima888@gmail.com>
2023-07-07 17:38:27 -0300

    First commit

---
+++ Source/PGC/PGCGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PGCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PGC_API APGCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
